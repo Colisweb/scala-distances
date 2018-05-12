3a736ecc2dc13dab652e8812938b92ac7b17e57b
jules Ivanic <jules.ivanic@gmail.com>
2018-05-12 14:34:59 +0200

    Make it compile

--- src/main/scala/com/guizmaii/distances/DistanceApi.scala
+++ src/main/scala/com/guizmaii/distances/DistanceApi.scala
@@ -14,6 +14,7 @@ class DistanceApi[AIO[_]: Par](distanceProvider: DistanceProvider[AIO], cachePro
 
   import DistanceApi._
   import cats.implicits._
+  import cats.temp.par._
   import com.guizmaii.distances.utils.RichImplicits._
 
   final def distance(
