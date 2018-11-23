4ed586b450f3ea1c1c4a0d82ddf95b4983834750
jules Ivanic <jules.ivanic@gmail.com>
2018-11-23 12:17:05 +0100

    Increase Google `GeoApiContext#queryRateLimit` from 100 to 1000

--- providers/google/src/main/scala/com/guizmaii/distances/providers/google/GoogleGeoApiContext.scala
+++ providers/google/src/main/scala/com/guizmaii/distances/providers/google/GoogleGeoApiContext.scala
@@ -7,22 +7,35 @@ import com.google.maps.GeoApiContext
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-final case class GoogleGeoApiContext(apiKey: String, connectTimeout: Duration, readTimeout: Duration) {
+final case class GoogleGeoApiContext(apiKey: String, connectTimeout: Duration, readTimeout: Duration, queryRateLimit: Int) {
 
   /**
     * More infos about the rate limit:
-    *   - https://developers.google.com/maps/documentation/distance-matrix/usage-limits
+    *   - https://developers.google.com/maps/documentation/distance-matrix/usage-and-billing
+    *
+    * Useful information taken from this documentation at the date of 2018.11.23:
+    *
+    * ```
+    * Other Usage Limits
+    * ------------------
+    * While you are no longer limited to a maximum number of elements per day (EPD), the following usage limits are still in place for the Distance Matrix API:
+    *
+    *  - Maximum of 25 origins or 25 destinations per request.
+    *  - Maximum 100 elements that include Departure Time, per request.
+    *  - Maximum 100 elements per client-side request.
+    *  - 1000 elements per second (EPS), calculated as the sum of client-side and server-side queries.
+    * ```
     */
   final val geoApiContext: GeoApiContext =
     new GeoApiContext.Builder()
       .apiKey(apiKey)
       .connectTimeout(connectTimeout.toMillis, TimeUnit.MILLISECONDS)
       .readTimeout(readTimeout.toMillis, TimeUnit.MILLISECONDS)
-      .queryRateLimit(100)
+      .queryRateLimit(queryRateLimit)
       .build()
 
 }
 
 object GoogleGeoApiContext {
-  final def apply(googleApiKey: String): GoogleGeoApiContext = new GoogleGeoApiContext(googleApiKey, 1 second, 1 second)
+  final def apply(googleApiKey: String): GoogleGeoApiContext = new GoogleGeoApiContext(googleApiKey, 1 second, 1 second, 1000)
 }
