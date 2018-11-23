93417138dad153a81043f158ff3be78c50991174
jules Ivanic <jules.ivanic@gmail.com>
2018-11-23 11:36:42 +0100

    Clean

--- caches/redis/src/main/scala/com/guizmaii/distances/caches/RedisCache.scala
+++ caches/redis/src/main/scala/com/guizmaii/distances/caches/RedisCache.scala
@@ -1,6 +1,7 @@
 package com.guizmaii.distances.caches
 
 import cats.effect
+import cats.effect.Async
 import com.guizmaii.distances.Cache
 import io.circe.Json
 import scalacache.{Cache => InnerCache}
@@ -11,7 +12,7 @@ object RedisCache {
 
   import scalacache.redis.{RedisCache => InnerRedisCache}
 
-  final def apply[F[_]: effect.Async](config: RedisConfiguration, ttl: Option[Duration]): Cache[F] =
+  final def apply[F[_]: Async](config: RedisConfiguration, ttl: Option[Duration]): Cache[F] =
     new Cache[F](ttl) {
       import scalacache.serialization.circe._
       override private[distances] implicit final val innerCache: InnerCache[Json] = InnerRedisCache[Json](config.jedisPool)
