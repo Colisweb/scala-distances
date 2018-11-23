cd56e3e2d04d63616703aa9c84de5517d92b2fbd
jules Ivanic <jules.ivanic@gmail.com>
2018-11-23 10:58:37 +0100

    Clean

--- core/src/main/scala/com/guizmaii/distances/Cache.scala
+++ core/src/main/scala/com/guizmaii/distances/Cache.scala
@@ -14,10 +14,7 @@ abstract class Cache[F[_]: Async](ttl: Option[Duration]) {
   private[distances] implicit val innerCache: InnerCache[Json]
 
   // TODO Jules: An optimization is possible when there's a cache miss because in that case the deserialization is useless.
-  private[distances] def cachingF[V](keyParts: Any*)(f: => F[V])(
-      implicit decoder: Decoder[V],
-      encoder: Encoder[V]
-  ): F[V] =
+  private[distances] def cachingF[V](keyParts: Any*)(f: => F[V])(implicit decoder: Decoder[V], encoder: Encoder[V]): F[V] =
     innerCache
       .cachingF(keyParts: _*)(ttl)(f.map(encoder.apply))
       .flatMap(json => Async[F].fromEither(decoder.decodeJson(json)))
