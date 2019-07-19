897e59e14254617d3ee613881ce86fb374bd61a5
Simon Bar <simon.b@colisweb.com>
2019-07-19 11:48:22 +0200

    Fix tests by mocking DistanceF

--- tests/src/test/scala/com/colisweb/distances/utils/Stubs.scala
+++ tests/src/test/scala/com/colisweb/distances/utils/Stubs.scala
@@ -2,12 +2,16 @@ package com.colisweb.distances.utils
 
 import java.time.Instant
 
+import cats.Monad
 import cats.effect.Async
-import com.colisweb.distances.DistanceProvider
-import com.colisweb.distances.Types.TravelMode
-import com.colisweb.distances.Types.LatLong
-import com.colisweb.distances._
+import com.colisweb.distances.Types.{Distance, LatLong, TravelMode}
+import com.colisweb.distances.{DistanceProvider, _}
 import com.colisweb.distances.caches.NoCache
+import squants.motion.KilometersPerHour
+import squants.space.Meters
+
+import scala.concurrent.duration._
+import scala.math._
 
 object Stubs {
 
@@ -25,5 +29,41 @@ object Stubs {
   }
 
   def geocoderStub[F[_]: Async] = new Geocoder[F](geoProviderStub, NoCache())
+
+  private def haversine(origin: LatLong, destination: LatLong): Double = {
+    val deltaLat = toRadians(destination.latitude - origin.latitude)
+    val deltaLon = toRadians(destination.longitude - origin.longitude)
+
+    val hav = pow(sin(deltaLat / 2), 2) + cos(toRadians(origin.latitude)) * cos(
+      toRadians(destination.latitude)
+    ) * pow(sin(deltaLon / 2), 2)
+    val greatCircleDistance = 2 * atan2(sqrt(hav), sqrt(1 - hav))
+
+    val earthRadiusMiles  = 3958.761
+    val earthRadiusMeters = earthRadiusMiles / 0.00062137
+
+    earthRadiusMeters * greatCircleDistance
+  }
+
+  def mockedDistanceF[F[_]: Monad](
+      mode: TravelMode,
+      origin: LatLong,
+      destination: LatLong,
+      maybeDepartureTime: Option[Instant]
+  ): F[Distance] = {
+    Monad[F].pure {
+      val distance       = Meters(haversine(origin, destination).round)
+      val travelDuration = (distance / KilometersPerHour(50)).toSeconds.seconds
+      val trafficDuration = mode match {
+        case TravelMode.Driving => 5.minutes
+        case _                  => 0.minute
+      }
+
+      maybeDepartureTime match {
+        case Some(_) => Distance(distance, travelDuration + trafficDuration)
+        case None    => Distance(distance, travelDuration)
+      }
+    }
+  }
 
 }
