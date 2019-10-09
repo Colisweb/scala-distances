54391906be31f25f80a835d17777865249bdd3b9
Cyril Verdier <cyril.verdier@colisweb.com>
2019-10-09 10:56:16 +0200

    wip

--- core/src/main/scala/com/colisweb/distances/re/AdaptError.scala
+++ core/src/main/scala/com/colisweb/distances/re/AdaptError.scala
@@ -13,8 +13,20 @@ class AdaptError[F[_]: Functor, E, E2, O](
     api.distance(path).map(_.leftMap(adaptation))
 }
 
+class AdaptErrorBatch[F[_]: Functor, E, E2, O](
+    private val api: DistanceBatchApi[F, E, O],
+    private val adaptation: E => E2
+) extends DistanceBatchApi[F, E2, O] {
+
+  override def distances(paths: List[Path[O]]): F[Map[Path[O], Either[E2, DistanceAndDuration]]] =
+    api.distances(paths).map(_.mapValues(_.leftMap(adaptation)))
+}
+
 object AdaptError {
-  def apply[F[_]: Functor, E, E2, O](api: DistanceApi[F, E, O], adaptation: E => E2): AdaptError[F, E, E2, O] =
+
+  def apply[F[_]: Functor, E, E2, O](api: DistanceApi[F, E, O], adaptation: E => E2): DistanceApi[F, E2, O] =
     new AdaptError(api, adaptation)
 
+  def apply[F[_]: Functor, E, E2, O](api: DistanceBatchApi[F, E, O], adaptation: E => E2): DistanceBatchApi[F, E2, O] =
+    new AdaptErrorBatch(api, adaptation)
 }
--- core/src/main/scala/com/colisweb/distances/re/cache/DistanceUpdateCache.scala
+++ core/src/main/scala/com/colisweb/distances/re/cache/DistanceUpdateCache.scala
@@ -1,7 +1,7 @@
 package com.colisweb.distances.re.cache
-import cats.Monad
+import cats.{Monad, Parallel}
 import cats.implicits._
-import com.colisweb.distances.re.DistanceApi
+import com.colisweb.distances.re.{DistanceApi, DistanceBatchApi}
 import com.colisweb.distances.re.model.{DistanceAndDuration, Path}
 
 class DistanceUpdateCache[F[_]: Monad, E, O](
@@ -17,9 +17,55 @@ class DistanceUpdateCache[F[_]: Monad, E, O](
 }
 
 object DistanceUpdateCache {
+
   def apply[F[_]: Monad, E, O](
       api: DistanceApi[F, E, O],
       cacheSet: CacheSet[F, Path[O], DistanceAndDuration]
   ): DistanceUpdateCache[F, E, O] =
     new DistanceUpdateCache(api, cacheSet)
+}
+
+class DistanceUpdateCacheBatchSequential[F[_]: Monad, E, O](
+    private val api: DistanceBatchApi[F, E, O],
+    private val cacheSet: CacheSet[F, Path[O], DistanceAndDuration]
+) extends DistanceBatchApi[F, E, O] {
+
+  override def distances(paths: List[Path[O]]): F[Map[Path[O], Either[E, DistanceAndDuration]]] =
+    api.distances(paths).flatMap { results =>
+      results
+        .collect { case (path, Right(value)) => path -> value }
+        .toList
+        .traverse { case (path, value) => cacheSet.set(path, value) }
+        .map(_ => results)
+    }
+}
+
+class DistanceUpdateCacheBatchParallel[F[_]: Monad: Parallel, E, O](
+    private val api: DistanceBatchApi[F, E, O],
+    private val cacheSet: CacheSet[F, Path[O], DistanceAndDuration]
+) extends DistanceBatchApi[F, E, O] {
+
+  override def distances(paths: List[Path[O]]): F[Map[Path[O], Either[E, DistanceAndDuration]]] =
+    api.distances(paths).flatMap { results =>
+      results
+        .collect { case (path, Right(value)) => path -> value }
+        .toList
+        .parTraverse { case (path, value) => cacheSet.set(path, value) }
+        .map(_ => results)
+    }
+}
+
+object DistanceUpdateCacheBatch {
+
+  def sequential[F[_]: Monad, E, O](
+      api: DistanceBatchApi[F, E, O],
+      cacheSet: CacheSet[F, Path[O], DistanceAndDuration]
+  ): DistanceBatchApi[F, E, O] =
+    new DistanceUpdateCacheBatchSequential(api, cacheSet)
+
+  def parallel[F[_]: Monad: Parallel, E, O](
+      api: DistanceBatchApi[F, E, O],
+      cacheSet: CacheSet[F, Path[O], DistanceAndDuration]
+  ): DistanceBatchApi[F, E, O] =
+    new DistanceUpdateCacheBatchParallel(api, cacheSet)
 }
