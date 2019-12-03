b4e3f0eca90220e5a1a360b5246fb7486c684f31
Cyril Verdier <cyril.verdier@colisweb.com>
2019-12-03 12:00:55 +0100

    Scalacache in core, simplify implems

--- caches/redis/src/main/scala/com/colisweb/distances/caches/RedisCache.scala
+++ caches/redis/src/main/scala/com/colisweb/distances/caches/RedisCache.scala
@@ -1,66 +1,21 @@
 package com.colisweb.distances.caches
 
-import cats.Functor
-import cats.implicits._
-import com.colisweb.distances.caches.RedisCache.KeyBuilder
-import com.colisweb.distances.re.cache.{Cache, CacheGet, CacheSet}
-import scalacache.Mode
-import scalacache.redis.{RedisCache => RedisCacheApi}
+import com.colisweb.distances.re.cache.ScalaCacheCache
+import com.colisweb.distances.re.cache.ScalaCacheCache.Key
 import scalacache.serialization.Codec
+import scalacache.{Flags, Mode}
 
 import scala.concurrent.duration.FiniteDuration
 
-class RedisCacheGet[F[_]: Mode, K: KeyBuilder, V](
-    cacheApi: RedisCacheApi[V]
-) extends CacheGet[F, K, V] {
-
-  override def get(key: K): F[Option[V]] =
-    cacheApi.get[F](KeyBuilder[K].parts(key))
-}
-
-class RedisCacheSet[F[_]: Functor: Mode, K: KeyBuilder, V](
-    cacheApi: RedisCacheApi[V],
-    ttl: Option[FiniteDuration]
-) extends CacheSet[F, K, V] {
-
-  override def set(key: K, value: V): F[Unit] =
-    cacheApi.put(KeyBuilder[K].parts(key))(value, ttl).map(_ => ())
-}
-
-class RedisCache[F[_]: Functor: Mode, K, V](
-    cacheGet: CacheGet[F, K, V],
-    cacheSet: CacheSet[F, K, V]
-) extends Cache[F, K, V] {
-
-  override def get(key: K): F[Option[V]]      = cacheGet.get(key)
-  override def set(key: K, value: V): F[Unit] = cacheSet.set(key, value)
-}
-
 object RedisCache {
+  import scalacache.redis.{RedisCache => RedisScalaCache}
 
-  trait KeyBuilder[K] {
-    def parts(key: K): Seq[Any]
-  }
-  object KeyBuilder {
-    def apply[K](implicit KP: KeyBuilder[K]): KeyBuilder[K] = KP
-  }
-  def keyBuilder[K](builder: K => Seq[Any]): KeyBuilder[K] = builder.apply
-  def productKeyBuilder[K <: Product]: KeyBuilder[K]       = _.productIterator.toSeq
-
-  def getOnly[F[_]: Mode, K: KeyBuilder, V: Codec](config: RedisConfiguration): RedisCacheGet[F, K, V] =
-    new RedisCacheGet[F, K, V](RedisCacheApi(config.jedisPool))
-
-  def setOnly[F[_]: Functor: Mode, K: KeyBuilder, V: Codec](
+  def apply[F[_]: Mode, K: Key, V: Codec](
       config: RedisConfiguration,
+      flags: Flags,
       ttl: Option[FiniteDuration]
-  ): RedisCacheSet[F, K, V] =
-    new RedisCacheSet[F, K, V](RedisCacheApi(config.jedisPool), ttl)
-
-  def apply[F[_]: Functor: Mode, K: KeyBuilder, V: Codec](
-      config: RedisConfiguration,
-      ttl: Option[FiniteDuration]
-  ): RedisCache[F, K, V] = {
-    val innerCache = RedisCacheApi[V](config.jedisPool)
-    new RedisCache[F, K, V](new RedisCacheGet(innerCache), new RedisCacheSet(innerCache, ttl))
+  ): ScalaCacheCache[F, K, V] = {
+    val redis = RedisScalaCache[V](config.jedisPool)
+    ScalaCacheCache(redis, flags, ttl)
   }
 }
---
+++ core/src/main/scala/com/colisweb/distances/re/cache/ScalaCacheCache.scala
@@ -0,0 +1,38 @@
+package com.colisweb.distances.re.cache
+
+import com.colisweb.distances.re.cache.ScalaCacheCache.Key
+import scalacache.{CacheAlg, Flags, Mode}
+
+import scala.concurrent.duration.FiniteDuration
+
+class ScalaCacheCache[F[_]: Mode, K: Key, V](cache: CacheAlg[V], flags: Flags, ttl: Option[FiniteDuration])
+    extends Cache[F, K, V] {
+  import Key.KeyOps
+  private implicit val implicitFlags: Flags = flags
+
+  override def get(key: K): F[Option[V]]     = cache.get(key.parts)
+  override def put(key: K, value: V): F[Any] = cache.put(key.parts)(value, ttl)
+}
+
+object ScalaCacheCache {
+
+  trait Key[K] {
+    def parts(key: K): Seq[Any]
+  }
+  object Key {
+    def apply[K](implicit KP: Key[K]): Key[K] = KP
+    implicit class KeyOps[K: Key](key: K) {
+      def parts: Seq[Any] = Key[K].parts(key)
+    }
+
+    def fromFunction[K](builder: K => Seq[Any]): Key[K] = builder.apply
+    def forProduct[K <: Product]: Key[K]                = _.productIterator.toSeq
+  }
+
+  def apply[F[_]: Mode, K: Key, V](
+      cache: CacheAlg[V],
+      flags: Flags,
+      ttl: Option[FiniteDuration]
+  ): ScalaCacheCache[F, K, V] =
+    new ScalaCacheCache(cache, flags, ttl)
+}
