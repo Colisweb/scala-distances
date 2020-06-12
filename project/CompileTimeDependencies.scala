6d74e51117a0229365020f73f9b62198910cf1a5
Scala Steward <gitlab@colisweb.com>
2020-06-12 01:11:08 +0000

    Update circe-generic-extras to 0.12.2

--- project/CompileTimeDependencies.scala
+++ project/CompileTimeDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   lazy val cats               = "2.0.0"
-  lazy val circe              = "0.12.1"
+  lazy val circe              = "0.12.2"
   lazy val circeOptics        = "0.12.0"
   lazy val enumeratum         = "1.6.1"
   lazy val google             = "0.14.0"
