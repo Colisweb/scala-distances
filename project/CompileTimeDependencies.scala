66e34d578dc25813279f8deb39c3c0dc26995e3c
Scala Steward <gitlab@colisweb.com>
2020-11-03 08:34:12 +0000

    Update pureconfig to 0.14.0

--- project/CompileTimeDependencies.scala
+++ project/CompileTimeDependencies.scala
@@ -9,7 +9,7 @@ object Versions {
   lazy val google             = "0.15.0"
   lazy val loggingInterceptor = "4.8.0"
   lazy val monix              = "3.2.2"
-  lazy val pureconfig         = "0.13.0"
+  lazy val pureconfig         = "0.14.0"
   lazy val scalaCache         = "0.28.0"
   lazy val scalaCompat        = "2.2.0"
   lazy val squants            = "1.7.0"
