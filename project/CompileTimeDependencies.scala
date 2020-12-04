a2a4dfc855c9fcdc5633af21b4064ca7848d79c1
Scala Steward <gitlab@colisweb.com>
2020-12-04 16:12:11 +0000

    Update cats-core, cats-effect to 2.3.0

--- project/CompileTimeDependencies.scala
+++ project/CompileTimeDependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Versions {
-  lazy val cats               = "2.2.0"
-  lazy val catsEffect         = "2.2.0"
+  lazy val cats               = "2.3.0"
+  lazy val catsEffect         = "2.3.0"
   lazy val circe              = "0.13.0"
   lazy val circeOptics        = "0.13.0"
   lazy val enumeratum         = "1.6.1"
