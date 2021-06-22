5324b58a1ee7538b03b68fcf3e2707b6924600bd
colas.mombrun <colas.mombrun@colisweb.com>
2021-06-22 10:16:30 +0200

    fix: add a copy method

--- core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
+++ core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
@@ -4,22 +4,35 @@ import enumeratum.{Enum, EnumEntry}
 
 import scala.collection.immutable
 
-sealed trait TravelMode extends EnumEntry { def maxSpeed: SpeedInKmH }
+sealed trait TravelMode extends EnumEntry {
+  def maxSpeed: SpeedInKmH
+  def copy(newSpeed: SpeedInKmH): TravelMode
+}
 
 object TravelMode extends Enum[TravelMode] {
   val values: immutable.IndexedSeq[TravelMode] = findValues
   private val defaultMaxSpeed: SpeedInKmH      = 250d
 
-  case class Car(maxSpeed: SpeedInKmH = defaultMaxSpeed) extends TravelMode
+  case class Car(maxSpeed: SpeedInKmH = defaultMaxSpeed) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Car(newSpeed)
+  }
   case class Truck(
       maxSpeed: SpeedInKmH = defaultMaxSpeed,
       weight: Option[WeightInKg] = None,
       length: Option[DimensionInCm] = None,
       width: Option[DimensionInCm] = None,
       height: Option[DimensionInCm] = None
-  )                                                             extends TravelMode
-  case class Scooter(maxSpeed: SpeedInKmH = defaultMaxSpeed)    extends TravelMode
-  case class Pedestrian(maxSpeed: SpeedInKmH = defaultMaxSpeed) extends TravelMode
-  case class Bicycle(maxSpeed: SpeedInKmH = defaultMaxSpeed)    extends TravelMode
+  ) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Truck(newSpeed, weight, length, width, height)
+  }
+  case class Scooter(maxSpeed: SpeedInKmH = defaultMaxSpeed) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Scooter(newSpeed)
+  }
+  case class Pedestrian(maxSpeed: SpeedInKmH = defaultMaxSpeed) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Pedestrian(newSpeed)
+  }
+  case class Bicycle(maxSpeed: SpeedInKmH = defaultMaxSpeed) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Bicycle(newSpeed)
+  }
 
 }
