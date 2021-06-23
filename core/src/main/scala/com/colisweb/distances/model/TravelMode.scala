482140a0387751987eec32d4295dd2fb90f8dd37
colas.mombrun <colas.mombrun@colisweb.com>
2021-06-23 12:05:26 +0200

    fix: override entryname to avoid TravelMode(Speed) being the entryName output

--- core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
+++ core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
@@ -13,6 +13,8 @@ object TravelMode extends Enum[TravelMode] {
   val values: immutable.IndexedSeq[TravelMode] = findValues
 
   case class Car(maxSpeed: SpeedInKmH = 130d) extends TravelMode {
+    override val entryName: String                      = "car"
+
     override def copy(newSpeed: SpeedInKmH): TravelMode = Car(newSpeed)
   }
   case class Truck(
@@ -22,15 +24,24 @@ object TravelMode extends Enum[TravelMode] {
       width: Option[DimensionInCm] = None,
       height: Option[DimensionInCm] = None
   ) extends TravelMode {
+    override val entryName: String                      = "truck"
+
     override def copy(newSpeed: SpeedInKmH): TravelMode = Truck(newSpeed, weight, length, width, height)
   }
   case class Scooter(maxSpeed: SpeedInKmH = 110d) extends TravelMode {
+    override val entryName: String                      = "scooter"
+
     override def copy(newSpeed: SpeedInKmH): TravelMode = Scooter(newSpeed)
   }
   case class Pedestrian(maxSpeed: SpeedInKmH = 10d) extends TravelMode {
+    override val entryName: String                      = "pedestrian"
+
     override def copy(newSpeed: SpeedInKmH): TravelMode = Pedestrian(newSpeed)
+
   }
   case class Bicycle(maxSpeed: SpeedInKmH = 30d) extends TravelMode {
+    override val entryName: String                      = "bicycle"
+
     override def copy(newSpeed: SpeedInKmH): TravelMode = Bicycle(newSpeed)
   }
 
