8f3a4d03acbe706668e167abf53f181983f6c634
colas.mombrun <colas.mombrun@colisweb.com>
2021-06-23 16:54:20 +0200

    chore: reert previous. It is the caller job to adapt to the format

--- core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
+++ core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
@@ -1,20 +1,13 @@
 package com.colisweb.distances.model
 
-import enumeratum.{Enum, EnumEntry}
-
-import scala.collection.immutable
-
-sealed trait TravelMode extends EnumEntry {
+sealed trait TravelMode {
   def maxSpeed: SpeedInKmH
   def copy(newSpeed: SpeedInKmH): TravelMode
 }
 
-object TravelMode extends Enum[TravelMode] {
-  val values: immutable.IndexedSeq[TravelMode] = findValues
+object TravelMode {
 
   case class Car(maxSpeed: SpeedInKmH = 130d) extends TravelMode {
-    override val entryName: String                      = "car"
-
     override def copy(newSpeed: SpeedInKmH): TravelMode = Car(newSpeed)
   }
   case class Truck(
@@ -24,24 +17,16 @@ object TravelMode extends Enum[TravelMode] {
       width: Option[DimensionInCm] = None,
       height: Option[DimensionInCm] = None
   ) extends TravelMode {
-    override val entryName: String                      = "truck"
-
     override def copy(newSpeed: SpeedInKmH): TravelMode = Truck(newSpeed, weight, length, width, height)
   }
   case class Scooter(maxSpeed: SpeedInKmH = 110d) extends TravelMode {
-    override val entryName: String                      = "scooter"
-
     override def copy(newSpeed: SpeedInKmH): TravelMode = Scooter(newSpeed)
   }
   case class Pedestrian(maxSpeed: SpeedInKmH = 10d) extends TravelMode {
-    override val entryName: String                      = "pedestrian"
-
     override def copy(newSpeed: SpeedInKmH): TravelMode = Pedestrian(newSpeed)
 
   }
   case class Bicycle(maxSpeed: SpeedInKmH = 30d) extends TravelMode {
-    override val entryName: String                      = "bicycle"
-
     override def copy(newSpeed: SpeedInKmH): TravelMode = Bicycle(newSpeed)
   }
 
