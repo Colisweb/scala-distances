7038970baaeb2462a1828921bdf51ea451d4e946
Scala Steward <gitlab@colisweb.com>
2021-08-27 22:07:23 +0000

    Reformat with scalafmt 3.0.1

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -9,12 +9,12 @@ object TestVersions {
 }
 
 object TestDependencies {
-  final lazy val kantan               = "com.nrinaudo"      %% "kantan.csv"               % TestVersions.kantan % Test
-  final lazy val kantanCats           = "com.nrinaudo"      %% "kantan.csv-cats"          % TestVersions.kantan % Test
-  final lazy val kantanGeneric        = "com.nrinaudo"      %% "kantan.csv-generic"       % TestVersions.kantan % Test
-  final lazy val circeLiteral         = "io.circe"          %% "circe-literal"            % Versions.circe % Test
-  final lazy val scalacheck           = "org.scalacheck"    %% "scalacheck"               % TestVersions.scalacheck % Test
-  final lazy val scalatestPlus        = "org.scalatestplus" %% "scalatestplus-scalacheck" % TestVersions.scalatestPlus % Test
-  final lazy val scalatest            = "org.scalatest"     %% "scalatest"                % TestVersions.scalatest % Test
-  final lazy val enumeratumScalacheck = "com.beachape"      %% "enumeratum-scalacheck"    % TestVersions.enumeratum % Test
+  final lazy val kantan        = "com.nrinaudo"      %% "kantan.csv"               % TestVersions.kantan        % Test
+  final lazy val kantanCats    = "com.nrinaudo"      %% "kantan.csv-cats"          % TestVersions.kantan        % Test
+  final lazy val kantanGeneric = "com.nrinaudo"      %% "kantan.csv-generic"       % TestVersions.kantan        % Test
+  final lazy val circeLiteral  = "io.circe"          %% "circe-literal"            % Versions.circe             % Test
+  final lazy val scalacheck    = "org.scalacheck"    %% "scalacheck"               % TestVersions.scalacheck    % Test
+  final lazy val scalatestPlus = "org.scalatestplus" %% "scalatestplus-scalacheck" % TestVersions.scalatestPlus % Test
+  final lazy val scalatest     = "org.scalatest"     %% "scalatest"                % TestVersions.scalatest     % Test
+  final lazy val enumeratumScalacheck = "com.beachape" %% "enumeratum-scalacheck" % TestVersions.enumeratum % Test
 }
