5b3102b1bc07e25e80d4a8b86cf152bfe0f67ad4
colas.mombrun <colas.mombrun@colisweb.com>
2021-08-27 10:41:43 +0200

    feat: make speed optional

--- core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
+++ core/src/main/scala/com/colisweb/distances/model/TravelMode.scala
@@ -1,33 +1,33 @@
 package com.colisweb.distances.model
 
 sealed trait TravelMode {
-  def maxSpeed: SpeedInKmH
+  def maxSpeed: Option[SpeedInKmH]
   def copy(newSpeed: SpeedInKmH): TravelMode
 }
 
 object TravelMode {
 
-  case class Car(maxSpeed: SpeedInKmH = 130d) extends TravelMode {
-    override def copy(newSpeed: SpeedInKmH): TravelMode = Car(newSpeed)
+  case class Car(maxSpeed: Option[SpeedInKmH] = None) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Car(Some(newSpeed))
   }
   case class Truck(
-      maxSpeed: SpeedInKmH = 110d,
+      maxSpeed: Option[SpeedInKmH] = None,
       weight: Option[WeightInKg] = None,
       length: Option[DimensionInCm] = None,
       width: Option[DimensionInCm] = None,
       height: Option[DimensionInCm] = None
   ) extends TravelMode {
-    override def copy(newSpeed: SpeedInKmH): TravelMode = Truck(newSpeed, weight, length, width, height)
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Truck(Some(newSpeed), weight, length, width, height)
   }
-  case class Scooter(maxSpeed: SpeedInKmH = 110d) extends TravelMode {
-    override def copy(newSpeed: SpeedInKmH): TravelMode = Scooter(newSpeed)
+  case class Scooter(maxSpeed: Option[SpeedInKmH] = None) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Scooter(Some(newSpeed))
   }
-  case class Pedestrian(maxSpeed: SpeedInKmH = 10d) extends TravelMode {
-    override def copy(newSpeed: SpeedInKmH): TravelMode = Pedestrian(newSpeed)
+  case class Pedestrian(maxSpeed: Option[SpeedInKmH] = None) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Pedestrian(Some(newSpeed))
 
   }
-  case class Bicycle(maxSpeed: SpeedInKmH = 30d) extends TravelMode {
-    override def copy(newSpeed: SpeedInKmH): TravelMode = Bicycle(newSpeed)
+  case class Bicycle(maxSpeed: Option[SpeedInKmH] = None) extends TravelMode {
+    override def copy(newSpeed: SpeedInKmH): TravelMode = Bicycle(Some(newSpeed))
   }
 
 }
--- providers/here/src/main/scala/com/colisweb/distances/providers/here/HereAdaptor.scala
+++ providers/here/src/main/scala/com/colisweb/distances/providers/here/HereAdaptor.scala
@@ -8,27 +8,25 @@ object HereAdaptor {
     def asHere: Map[String, String] =
       mode match {
         case mode: TravelMode.Car =>
-          Map("transportMode" -> "car", "vehicle[speedCap]" -> s"${mode.maxSpeed / 3.6}")
+          val speed = mode.maxSpeed.map(s => "vehicle[speedCap]" -> s"${s / 3.6}")
+          Map("transportMode" -> "car") ++ speed
+
         case mode: TravelMode.Truck =>
           val weight = mode.weight.map(w => "truck[grossWeight]" -> w.toString)
           val height = mode.height.map(h => "truck[height]" -> h.toString)
           val length = mode.length.map(l => "truck[length]" -> l.toString)
           val width  = mode.width.map(w => "truck[width]" -> w.toString)
-          Map(
-            "transportMode"     -> "truck",
-            "vehicle[speedCap]" -> s"${mode.maxSpeed / 3.6}"
-          ) ++ weight ++ height ++ width ++ length
+          val speed  = mode.maxSpeed.map(s => "vehicle[speedCap]" -> s"${s / 3.6}")
+          Map("transportMode" -> "truck") ++ weight ++ height ++ width ++ length ++ speed
 
         case mode: TravelMode.Scooter =>
-          Map(
-            "transportMode"     -> "scooter",
-            "vehicle[speedCap]" -> s"${mode.maxSpeed / 3.6}"
-          )
+          val speed = mode.maxSpeed.map(s => "vehicle[speedCap]" -> s"${s / 3.6}")
+          Map("transportMode" -> "scooter") ++ speed
+
         case mode: TravelMode.Pedestrian =>
-          Map(
-            "transportMode"     -> "pedestrian",
-            "pedestrian[speed]" -> s"${mode.maxSpeed / 3.6}"
-          )
+          val speed = mode.maxSpeed.map(s => "pedestrian[speed]" -> s"${s / 3.6}")
+          Map("transportMode" -> "pedestrian") ++ speed
+
         case _: TravelMode.Bicycle =>
           Map("transportMode" -> "bicycle")
       }
