92080b45e163682bbc4797b22dfff0f4acb45c0e
Scala Steward <gitlab@colisweb.com>
2022-03-11 23:08:22 +0000

    Update mockito-scala-scalatest to 1.17.5

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object TestVersions {
-  final val mockitoScala  = "1.16.55"
+  final val mockitoScala  = "1.17.5"
   final val kantan        = "0.6.1"
   final val scalacheck    = "1.15.4"
   final val scalatest     = "3.2.11"
