9cf783fc6835dc3534969690b6c49a4b9a247026
Scala Steward <gitlab@colisweb.com>
2023-01-06 23:10:21 +0000

    Update scalatest to 3.2.15

--- project/TestDependencies.scala
+++ project/TestDependencies.scala
@@ -4,7 +4,7 @@ object TestVersions {
   final val mockitoScala  = "1.17.12"
   final val kantan        = "0.7.0"
   final val scalacheck    = "1.17.0"
-  final val scalatest     = "3.2.13"
+  final val scalatest     = "3.2.15"
   final val scalatestPlus = "3.1.0.0-RC2"
   final val enumeratum    = "1.7.0"
 }
