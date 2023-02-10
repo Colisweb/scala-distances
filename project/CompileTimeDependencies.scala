976cc6bc5a2f98ebc69453e84ba56234a3911466
Scala Steward <gitlab@colisweb.com>
2023-02-10 23:12:53 +0000

    Update circe-generic-extras to 0.14.3

--- project/CompileTimeDependencies.scala
+++ project/CompileTimeDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   final val catsEffect         = "2.3.3"
-  final val circe              = "0.14.2"
+  final val circe              = "0.14.3"
   final val circeOptics        = "0.14.1"
   final val enumeratum         = "1.7.0"
   final val google             = "2.0.0"
